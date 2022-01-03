ed4c72c8f5e4dd74d16a10d3a314d5803d1cbf9d
ceasaro <ceesvw@gmail.com>
2022-01-03 13:17:12 +0100

    Introduces ProducedProducts and Resource

--- app/satisfactory/production/models.py
+++ app/satisfactory/production/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 
 
@@ -15,8 +17,12 @@ class BaseCodeModel(BaseModel):
     code = models.CharField(unique=True, max_length=32)
 
 
-class Resource(BaseCodeModel):
+class Product(BaseCodeModel):
     name = models.CharField(max_length=255)
+
+    @property
+    def is_base_resource(self):
+        return Factory.objects.filter(produced_products__product=self, resources__isnull=True).exists()
 
     def __str__(self):
         return f"{self.name} ({self.code})"
@@ -29,10 +35,37 @@ class Construction(BaseModel):
 
 
 class Factory(Construction):
-    produces = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Factory of '{', '.join([str(p) for p in self.produces])}'"
+
+    @property
+    def produces(self):
+        return [p.product for p in self.produced_products.all()]
+
+    @property
+    def requires(self):
+        return [r.resource for r in self.resources.all()]
+
+    def add_product(self, product, amount, production_time=timedelta(minutes=1)):
+        ProducedProducts.objects.create(product=product, factory=self, amount=amount, production_time=production_time)
+
+    def add_resource(self, resource, amount, production_time=timedelta(minutes=1)):
+        Resource.objects.create(resource=resource, factory=self, amount=amount, process_time=production_time)
+
+    def resources(self):
+        pass
+
+
+class ProducedProducts(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='produced')
+    factory = models.ForeignKey(Factory, on_delete=models.CASCADE, related_name='produced_products')
     amount = models.IntegerField()
     production_time = models.DurationField()
 
-    def __str__(self):
-        return f"Factory of '{self.produces}'"
 
+class Resource(models.Model):
+    resource = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='used_by')
+    factory = models.ForeignKey(Factory, on_delete=models.CASCADE, related_name='resources')
+    amount = models.IntegerField()
+    process_time = models.DurationField()
--- app/satisfactory/production/tests/test_factories.py
+++ app/satisfactory/production/tests/test_factories.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 import pytest
 
-from satisfactory.production.models import Factory, Resource
+from satisfactory.production.models import Factory, Product, ProducedProducts
 
 
 def test_sanity():
@@ -11,7 +11,19 @@ def test_sanity():
 
 @pytest.mark.django_db
 def test_factory():
-    iron = Resource.objects.create(name='Iron', code='Fe')
-    iron_factory = Factory.objects.create(produces=iron, amount=30, production_time=timedelta(minutes=1))
-    print(iron)
-    print(iron_factory)
+    iron = Product.objects.create(name='Iron', code='Fe')
+    pipe = Product.objects.create(name='Iron pipe', code='Fe-pipe')
+    iron_factory = Factory.objects.create()
+    iron_factory.add_product(product=iron, amount=30)
+
+    pipe_factory = Factory.objects.create()
+    pipe_factory.add_resource(resource=iron, amount=15)
+    pipe_factory.add_product(product=pipe, amount=15)
+
+    assert iron_factory.produces == [iron]
+    assert iron_factory.requires == []
+    assert pipe_factory.produces == [pipe]
+    assert pipe_factory.requires == [iron]
+
+    assert iron.is_base_resource is True
+    assert pipe.is_base_resource is False
