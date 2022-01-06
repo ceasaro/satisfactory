bdfdef7d3ede1f26ab8258b513e626d92d48a850
ceasaro <ceesvw@gmail.com>
2022-01-06 18:16:41 +0100

    added GDAL and GEOS library paths

--- satisfactory/settings/settings.py
+++ satisfactory/settings/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,6 @@ STATIC_ROOT = BASE_DIR / 'static_root/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
