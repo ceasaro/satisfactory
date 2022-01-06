90750def57218431645cad827bc25bc4b9957257
ceasaro <ceesvw@gmail.com>
2022-01-06 17:26:43 +0100

    set secret

--- app/satisfactory/settings/settings.py
+++ app/satisfactory/settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5v-h!*q@+!2xkx*c$=^7pk39yqb)k&+(c@botgt7s%^nn#@i()'
+SECRET_KEY = '5v-h!*q@+!2xkx*c$=^7pk39yqb)k&+(c@botgt7s%^nn#@i()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
