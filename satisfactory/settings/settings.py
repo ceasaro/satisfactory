96866b205e658c85cccabca2444438d97d9ebaf3
ceasaro <ceesvw@gmail.com>
2022-01-08 10:57:03 +0100

    renamed database

--- satisfactory/settings/settings.py
+++ satisfactory/settings/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'satisfactory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'ghaev',
+        'NAME': 'satisfactory',
     }
 }
 
